24d9dd0a9469f02037bfa289e1bf969401615f9c
pedromarino07 <orgno123@gmail.com>
2026-03-23 12:32:52 -0300

    Atualização de banco e subir no reender

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,20 +5,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
-});
-
-pool.on('connect', () => {
-  console.log('Conectado ao banco de dados PostgreSQL');
-});
-
-pool.on('error', (err) => {
-  console.error('Erro inesperado no cliente do banco de dados', err);
-  process.exit(-1);
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
